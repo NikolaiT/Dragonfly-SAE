f1d66c2c899600f660d1b229949fd5b15f37963e
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2018-10-11 16:14:02 +0200

    first commit

---
+++ dragonfly_implementation.py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python3
+
+"""
+Implements the Dragonfly (SAE) handshake.
+
+Instead of using a client (STA) and a access point (AP), we
+just programmatically create a peer to peer network of two participiants.
+Either party may initiate the SAE protocol, either party can be the client and server.
+
+In a mesh scenario, where two APs (two equals) are trying to establish a connection
+between each other and each one could have the role of supplicant or authenticator.
+
+SAE is build upon the Dragonfly Key Exchange, which is described in https://tools.ietf.org/html/rfc7664.
+
+https://stackoverflow.com/questions/31074172/elliptic-curve-point-addition-over-a-finite-field-in-python
+"""
+
+import hashlib
+import random
+import logging
+from collections import namedtuple
+
+
+logger = logging.getLogger('dragonfly')
+logger.setLevel(logging.INFO)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('dragonfly.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+
+Point = namedtuple("Point", "x y")
+# The point at infinity (origin for the group law).
+O = 'Origin'
+
+def lsb(x):
+    binary = bin(x).lstrip('0b')
+    return binary[0]
+
+def legendre(a, p):
+    return pow(a, (p - 1) // 2, p)
+
+
+# https://rosettacode.org/wiki/Tonelli-Shanks_algorithm#Python
+
+def tonelli_shanks(n, p):
+    assert legendre(n, p) == 1, "not a square (mod p)"
+    q = p - 1
+    s = 0
+    while q % 2 == 0:
+        q //= 2
+        s += 1
+    if s == 1:
+        return pow(n, (p + 1) // 4, p)
+    for z in range(2, p):
+        if p - 1 == legendre(z, p):
+            break
+    c = pow(z, q, p)
+    r = pow(n, (q + 1) // 2, p)
+    t = pow(n, q, p)
+    m = s
+    t2 = 0
+    while (t - 1) % p != 0:
+        t2 = (t * t) % p
+        for i in range(1, m):
+            if (t2 - 1) % p == 0:
+                break
+            t2 = (t2 * t2) % p
+        b = pow(c, 1 << (m - i - 1), p)
+        r = (r * b) % p
+        c = (b * b) % p
+        t = (t * c) % p
+        m = i
+    return r
+
+class Curve():
+    """
+    Mathematical operations on a Elliptic Curve.
+    """
+
+    def __init__(self, a, b, p):
+        self.a = a
+        self.b = b
+        self.p = p
+
+    def curve_equation(self, x):
+        """
+        We currently use the elliptic curve
+        NIST P-384
+        """
+        return (pow(x, 3) + (self.a * x) + self.b) % self.p
+
+    def is_quadratic_residue(self, x):
+        """
+        https://en.wikipedia.org/wiki/Euler%27s_criterion
+        Computes Legendre Symbol.
+        """
+        return pow(x, (self.p-1) // 2, self.p) == 1
+
+    def valid(self, P):
+        """
+        Determine whether we have a valid representation of a point
+        on our curve.  We assume that the x and y coordinates
+        are always reduced modulo p, so that we can compare
+        two points for equality with a simple ==.
+        """
+        if P == O:
+            return True
+        else:
+            return (
+                (P.y**2 - (P.x**3 + self.a*P.x + self.b)) % self.p == 0 and
+                0 <= P.x < self.p and 0 <= P.y < self.p)
+
+    def inv_mod_p(self, x):
+        """
+        Compute an inverse for x modulo p, assuming that x
+        is not divisible by p.
+        """
+        if x % self.p == 0:
+            raise ZeroDivisionError("Impossible inverse")
+        return pow(x, self.p-2, self.p)
+
+    def ec_inv(self, P):
+        """
+        Inverse of the point P on the elliptic curve y^2 = x^3 + ax + b.
+        """
+        if P == O:
+            return P
+        return Point(P.x, (-P.y) % self.p)
+
+    def ec_add(self, P, Q):
+        """
+        Sum of the points P and Q on the elliptic curve y^2 = x^3 + ax + b.
+        https://stackoverflow.com/questions/31074172/elliptic-curve-point-addition-over-a-finite-field-in-python
+        """
+        if not (self.valid(P) and self.valid(Q)):
+            raise ValueError("Invalid inputs")
+
+        # Deal with the special cases where either P, Q, or P + Q is
+        # the origin.
+        if P == O:
+            result = Q
+        elif Q == O:
+            result = P
+        elif Q == self.ec_inv(P):
+            result = O
+        else:
+            # Cases not involving the origin.
+            if P == Q:
+                dydx = (3 * P.x**2 + self.a) * self.inv_mod_p(2 * P.y)
+            else:
+                dydx = (Q.y - P.y) * self.inv_mod_p(Q.x - P.x)
+            x = (dydx**2 - P.x - Q.x) % self.p
+            y = (dydx * (P.x - x) - P.y) % self.p
+            result = Point(x, y)
+
+        # The above computations *should* have given us another point
+        # on the curve.
+        assert self.valid(result)
+        return result
+
+    def double_add_algorithm(self, scalar, P):
+        """
+        Double-and-Add Algorithm for Point Multiplication
+        Input: A scalar in the range 0-p and a point on the elliptic curve P
+        https://stackoverflow.com/questions/31074172/elliptic-curve-point-addition-over-a-finite-field-in-python
+        """
+        assert self.valid(P)
+
+        b = bin(scalar).lstrip('0b')
+        T = P
+        for i in b:
+            T = self.ec_add(T, T)
+            if i == '1':
+                T = self.ec_add(T, P)
+
+        assert self.valid(T)
+        return T
+
+class Peer:
+    """
+    Implements https://wlan1nde.wordpress.com/2018/09/14/wpa3-improving-your-wlan-security/
+
+    Take a ECC curve from here: https://safecurves.cr.yp.to/
+
+    Curve used is: NIST P-384
+
+    y^2 = x^3-3x+27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575
+    modulo p = 2^384 - 2^128 - 2^96 + 2^32 - 1
+    2000 NIST; also in SEC 2 and NSA Suite B
+
+    brainpoolP256t1
+    y^2 = x^3-3x+46214326585032579593829631435610129746736367449296220983687490401182983727876
+    modulo p = 76884956397045344220809746629001649093037950200943055203735601445031516197751
+    """
+
+    def __init__(self, password, mac_address, name):
+
+        self.name = name
+        self.password = password
+        self.mac_address = mac_address
+
+        # We currently use the elliptic curve
+        # NIST P-384
+        # self.p = pow(2, 384) - pow(2, 128) - pow(2, 96) + pow(2, 32) - 1
+        # self.a = -3
+        # self.b = 27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575
+        # self.curve = Curve(self.a, self.b, self.p)
+
+        # Try out brainpoolP256t1
+        self.p = 76884956397045344220809746629001649093037950200943055203735601445031516197751
+        self.a = -3
+        self.b = 46214326585032579593829631435610129746736367449296220983687490401182983727876
+        self.curve = Curve(self.a, self.b, self.p)
+
+    def initiate(self, other_mac, k=40):
+        """
+        See algorithm in https://tools.ietf.org/html/rfc7664
+        in section 3.2.1
+        """
+        self.other_mac = other_mac
+        found = 0
+        counter = 1
+        n = self.p.bit_length() + 64
+        while (found == 0) and (counter <= k):
+            base = self.compute_hashed_password(counter)
+            temp = self.key_derivation_function(n, base, 'Dragonfly Hunting And Pecking')
+            logger.debug('temp={}'.format(temp))
+            seed = (temp % (self.p - 1)) + 1
+            val = self.curve.curve_equation(seed)
+            if self.curve.is_quadratic_residue(val):
+                if found == 0:
+                    x = seed
+                    save = base
+                    found = 1
+                    logger.info('Got point after {} iterations'.format(counter))
+
+            counter = counter + 1
+
+        if found == 0:
+            logger.error('No valid point found after {} iterations'.format(k))
+        elif found == 1:
+            # https://crypto.stackexchange.com/questions/6777/how-to-calculate-y-value-from-yy-mod-prime-efficiently
+            # https://rosettacode.org/wiki/Tonelli-Shanks_algorithm
+            y = tonelli_shanks(self.curve.curve_equation(x), self.p)
+
+            logger.debug('y={}'.format(y))
+            logger.debug('save={}'.format(save))
+
+            save_int = int.from_bytes(save, byteorder='little')
+            if lsb(y) == lsb(save_int):
+                PE = Point(x, y)
+            else:
+                PE = Point(x, self.p - y)
+
+            logger.info('[{}] Got Point={} after {} iterations'.format(self.name, PE, k))
+            logger.info('[{}] Point is on curve: {}'.format(self.name, self.curve.valid(PE)))
+
+            self.PE = PE
+            assert self.curve.valid(self.PE)
+
+    def commit_exchange(self):
+        """
+        This is basically Diffie Hellman Key Exchange (or in our case ECCDH)
+
+        In the Commit Exchange, both sides commit to a single guess of the
+        password.  The peers generate a scalar and an element, exchange them
+        with each other, and process the other's scalar and element to
+        generate a common and shared secret.
+        """
+        # Otherwise, each party chooses two random numbers, private and mask
+        self.private = random.randrange(1, self.p)
+        self.mask = random.randrange(1, self.p)
+
+        logger.debug('[{}] private={}'.format(self.name, self.private))
+        logger.debug('[{}] mask={}'.format(self.name, self.mask))
+
+        # These two secrets and the Password Element are then used to construct
+        # the scalar and element:
+
+        self.scalar = (self.private + self.mask) % self.p
+
+        P = self.curve.double_add_algorithm(self.mask, self.PE)
+
+        # get the inverse of res
+        # −P = (x_p , p − y_p ).
+        self.element = self.curve.ec_inv(P)
+
+        assert self.curve.valid(self.element)
+
+        # If the scalar is less than two (2), the private and mask MUST be
+        # thrown away and new values generated.  Once a valid scalar and
+        # Element are generated, the mask is no longer needed and MUST be
+        # irretrievably destroyed.
+
+        if self.scalar < 2:
+            logger.error('Scalar is {}, regenerating...'.format(self.scalar))
+
+        # The peers exchange their scalar and Element and check the peer's
+        # scalar and Element, deemed peer-scalar and Peer-Element.  If the peer
+        # has sent an identical scalar and Element -- i.e., if scalar equals
+        # peer-scalar and Element equals Peer-Element -- it is sign of a
+        # reflection attack, and the exchange MUST be aborted.  If the values
+        # differ, peer-scalar and Peer-Element must be validated.
+
+        logger.info('[{}] Sending scalar and element to the Peer!'.format(self.name))
+        logger.info('[{}] Scalar={}'.format(self.name, self.scalar))
+        logger.info('[{}] Element={}'.format(self.name, self.element))
+
+        return self.scalar, self.element
+
+    def compute_shared_secret(self, peer_element, peer_scalar):
+        """
+        ss = F(scalar-op(private,
+                         element-op(peer-Element,
+                                    scalar-op(peer-scalar, PE))))
+
+        AP1: K = private(AP1) • (scal(AP2) • P(x, y) ◊ new_point(AP2))
+               = private(AP1) • private(AP2) • P(x, y)
+        AP2: K = private(AP2) • (scal(AP1) • P(x, y) ◊ new_point(AP1))
+               = private(AP2) • private(AP1) • P(x, y)
+
+        A shared secret element is computed using one’s rand and
+        the other peer’s element and scalar:
+        Alice: K = rand A • (scal B • PW + elemB )
+        Bob: K = rand B • (scal A • PW + elemA )
+        """
+        assert self.curve.valid(peer_element)
+
+        # If both the peer-scalar and Peer-Element are
+        # valid, they are used with the Password Element to derive a shared
+        # secret, ss:
+
+        # K = self.curve.double_add_algorithm(
+        #     self.private,
+        #         self.curve.double_add_algorithm(peer_scalar,
+        #                 self.curve.ec_add(self.PE, peer_element)
+        #         )
+        # )
+        #
+        # logger.info('[{}] Shared Secret ss={}'.format(self.name, K[0]))
+
+        K = self.curve.double_add_algorithm(self.private,
+                         self.curve.ec_add(peer_element,
+                                    self.curve.double_add_algorithm(peer_scalar, self.PE)))
+
+        logger.info('[{}] Shared Secret ss={}'.format(self.name, K[0]))
+
+
+    def key_derivation_function(self, n, base, seed):
+        """
+        B.5.1 Per-Message Secret Number Generation Using Extra Random Bits
+
+        Key derivation function from Section B.5.1 of [FIPS186-4]
+
+        The key derivation function, KDF, is used to produce a
+        bitstream whose length is equal to the length of the prime from the
+        group's domain parameter set plus the constant sixty-four (64) to
+        derive a temporary value, and the temporary value is modularly
+        reduced to produce a seed.
+        """
+        combined_seed = '{}{}'.format(base, seed).encode()
+
+        # base and seed concatenated are the input to the RGB
+        random.seed(combined_seed)
+
+        # Obtain a string of N+64 returned_bits from an RBG with a security strength of
+        # requested_security_strength or more.
+
+        randbits = random.getrandbits(n)
+        binary_repr = format(randbits, '0{}b'.format(n))
+
+        assert len(binary_repr) == n
+
+        logger.debug('Rand={}'.format(binary_repr))
+
+        # Convert returned_bits to the non-negative integer c (see Appendix C.2.1).
+        C = 0
+        for i in range(n):
+            if int(binary_repr[i]) == 1:
+                C += pow(2, n-i)
+
+        logger.debug('C={}'.format(C))
+
+        #k = (C % (n - 1)) + 1
+
+        k = C
+
+        logger.debug('k={}'.format(k))
+
+        return k
+
+    def compute_hashed_password(self, counter):
+        maxm = max(self.mac_address, self.other_mac)
+        minm = min(self.mac_address, self.other_mac)
+        message = '{}{}{}{}'.format(maxm, minm, self.password, counter).encode()
+        logger.debug('Message to hash is: {}'.format(message))
+        H = hashlib.sha256()
+        H.update(message)
+        digest = H.digest()
+        return digest
+
+
+def handshake():
+    mac1, mac2 = '44:67:2D:2C:91:A6', '44:37:2C:2F:91:36'
+    sta = Peer('abc1238', mac1, 'STA')
+    ap = Peer('abc1238', mac2, 'AP')
+
+    sta.initiate(mac2)
+    ap.initiate(mac1)
+
+    logger.info('Starting dragonfly commit exchange...\n')
+
+    scalar_sta, element_sta = sta.commit_exchange()
+    scalar_ap, element_ap = ap.commit_exchange()
+
+    logger.info('Computing shared secret...\n')
+
+    sta.compute_shared_secret(element_ap, scalar_ap)
+    ap.compute_shared_secret(element_sta, scalar_sta)
+
+if __name__ == '__main__':
+    handshake()
