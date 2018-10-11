748b0d6514c72244cbc44280b07b13e3ce41083d
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2018-10-11 23:57:57 +0200

    works

--- dragonfly_implementation.py
+++ dragonfly_implementation.py
@@ -14,6 +14,7 @@ SAE is build upon the Dragonfly Key Exchange, which is described in https://tool
 
 https://stackoverflow.com/questions/31074172/elliptic-curve-point-addition-over-a-finite-field-in-python
 """
+import time
 import hashlib
 import random
 import logging
@@ -179,7 +180,7 @@ class Curve():
 
         b = bin(scalar).lstrip('0b')
         T = P
-        for i in b:
+        for i in b[1:]:
             T = self.ec_add(T, T)
             if i == '1':
                 T = self.ec_add(T, P)
@@ -190,37 +191,46 @@ class Curve():
 class Peer:
     """
     Implements https://wlan1nde.wordpress.com/2018/09/14/wpa3-improving-your-wlan-security/
-
     Take a ECC curve from here: https://safecurves.cr.yp.to/
 
-    Curve used is: NIST P-384
-
+    Example: NIST P-384
     y^2 = x^3-3x+27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575
     modulo p = 2^384 - 2^128 - 2^96 + 2^32 - 1
     2000 NIST; also in SEC 2 and NSA Suite B
 
-    brainpoolP256t1
-    y^2 = x^3-3x+46214326585032579593829631435610129746736367449296220983687490401182983727876
-    modulo p = 76884956397045344220809746629001649093037950200943055203735601445031516197751
+    See here: https://www.rfc-editor.org/rfc/rfc5639.txt
+
+   Curve-ID: brainpoolP256r1
+      p =
+      A9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377
+      A =
+      7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9
+      B =
+      26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6
+      x =
+      8BD2AEB9CB7E57CB2C4B482FFC81B7AFB9DE27E1E3BD23C23A4453BD9ACE3262
+      y =
+      547EF835C3DAC4FD97F8461A14611DC9C27745132DED8E545C1D54C72F046997
+      q =
+      A9FB57DBA1EEA9BC3E660A909D838D718C397AA3B561A6F7901E0E82974856A7
+      h = 1
     """
 
     def __init__(self, password, mac_address, name):
-
         self.name = name
         self.password = password
         self.mac_address = mac_address
 
-        # We currently use the elliptic curve
-        # NIST P-384
-        self.p = pow(2, 384) - pow(2, 128) - pow(2, 96) + pow(2, 32) - 1
-        self.a = -3
-        self.b = 27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575
+        # Try out Curve-ID: brainpoolP256t1
+        self.p = int('A9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377', 16)
+        self.a = int('7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9', 16)
+        self.b = int('26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6', 16)
+        self.q = int('A9FB57DBA1EEA9BC3E660A909D838D718C397AA3B561A6F7901E0E82974856A7', 16)
         self.curve = Curve(self.a, self.b, self.p)
 
-        # Try out brainpoolP256t1
-        # self.p = 76884956397045344220809746629001649093037950200943055203735601445031516197751
-        # self.a = -3
-        # self.b = 46214326585032579593829631435610129746736367449296220983687490401182983727876
+        # A toy curve
+        # self.a, self.b, self.p = 2, 2, 17
+        # self.q = 19
         # self.curve = Curve(self.a, self.b, self.p)
 
     def initiate(self, other_mac, k=40):
@@ -245,7 +255,7 @@ class Peer:
                     save = base
                     found = 1
                     num_valid_points += 1
-                    logger.info('Got point after {} iterations'.format(counter))
+                    logger.debug('Got point after {} iterations'.format(counter))
 
             counter = counter + 1
 
@@ -258,7 +268,10 @@ class Peer:
 
             PE = Point(x, y)
 
-            logger.info('[{}] Got Point={} after {} iterations'.format(self.name, PE, k))
+            # check valid point
+            assert self.curve.curve_equation(x) == pow(y, 2, self.p)
+
+            logger.info('[{}] Using {}-th valid Point={}'.format(self.name, num_valid_points, PE))
             logger.info('[{}] Point is on curve: {}'.format(self.name, self.curve.valid(PE)))
 
             self.PE = PE
@@ -272,7 +285,21 @@ class Peer:
         password.  The peers generate a scalar and an element, exchange them
         with each other, and process the other's scalar and element to
         generate a common and shared secret.
+
+        If we go back to elliptic curves over the real numbers, there is a nice geometric
+        interpretation for the ECDLP: given a starting point P, we compute 2P, 3P, . . .,
+        d P = T , effectively hopping back and forth on the elliptic curve. We then publish
+        the starting point P (a public parameter) and the final point T (the public key). In
+        order to break the cryptosystem, an attacker has to figure out how often we “jumped”
+        on the elliptic curve. The number of hops is the secret d, the private key.
         """
+        # seed the PBG before picking a new random number
+        # random.seed(time.process_time())
+
+        # None or no argument seeds from current time or from an operating
+        # system specific randomness source if available.
+        random.seed()
+
         # Otherwise, each party chooses two random numbers, private and mask
         self.private = random.randrange(1, self.p)
         self.mask = random.randrange(1, self.p)
@@ -283,7 +310,35 @@ class Peer:
         # These two secrets and the Password Element are then used to construct
         # the scalar and element:
 
-        self.scalar = (self.private + self.mask) % self.p
+        # what is q?
+        # o  A point, G, on the elliptic curve, which serves as a generator for
+        #    the ECC group.  G is chosen such that its order, with respect to
+        #    elliptic curve addition, is a sufficiently large prime.
+        #
+        # o  A prime, q, which is the order of G, and thus is also the size of
+        #    the cryptographic subgroup that is generated by G.
+
+        # https://math.stackexchange.com/questions/331329/is-it-possible-to-compute-order-of-a-point-over-elliptic-curve
+        # In the elliptic Curve cryptography, it is said that the order of base point
+        # should be a prime number, and order of a point P is defined as k, where kP=O.
+
+        # Theorem 9.2.1 The points on an elliptic curve together with O
+        # have cyclic subgroups. Under certain conditions all points on an
+        # elliptic curve form a cyclic group.
+        # For this specific curve the group order is a prime and, according to Theo-
+        # rem 8.2.4, every element is primitive.
+
+        # Question: What is the order of our PE?
+        # the order must be p, since p is a prime
+
+        self.scalar = (self.private + self.mask) % self.q
+
+        # If the scalar is less than two (2), the private and mask MUST be
+        # thrown away and new values generated.  Once a valid scalar and
+        # Element are generated, the mask is no longer needed and MUST be
+        # irretrievably destroyed.
+        if self.scalar < 2:
+            raise ValueError('Scalar is {}, regenerating...'.format(self.scalar))
 
         P = self.curve.double_add_algorithm(self.mask, self.PE)
 
@@ -292,14 +347,6 @@ class Peer:
         self.element = self.curve.ec_inv(P)
 
         assert self.curve.valid(self.element)
-
-        # If the scalar is less than two (2), the private and mask MUST be
-        # thrown away and new values generated.  Once a valid scalar and
-        # Element are generated, the mask is no longer needed and MUST be
-        # irretrievably destroyed.
-
-        if self.scalar < 2:
-            logger.error('Scalar is {}, regenerating...'.format(self.scalar))
 
         # The peers exchange their scalar and Element and check the peer's
         # scalar and Element, deemed peer-scalar and Peer-Element.  If the peer
@@ -334,34 +381,53 @@ class Peer:
         of e.g. scal(AP1) • P(x, y) is added to the new_point(AP2) and afterwards
         multiplied by private(AP1).
         """
-        assert self.curve.valid(peer_element)
+        self.peer_element = peer_element
+        self.peer_scalar = peer_scalar
+        self.peer_mac = peer_mac
+
+        assert self.curve.valid(self.peer_element)
 
         # If both the peer-scalar and Peer-Element are
         # valid, they are used with the Password Element to derive a shared
         # secret, ss:
 
-        Z = self.curve.double_add_algorithm(peer_scalar, self.PE)
-        ZZ = self.curve.ec_add(peer_element, Z)
+        Z = self.curve.double_add_algorithm(self.peer_scalar, self.PE)
+        ZZ = self.curve.ec_add(self.peer_element, Z)
         K = self.curve.double_add_algorithm(self.private, ZZ)
 
-        k = K[0]
+        self.k = K[0]
 
-        logger.info('[{}] Shared Secret ss={}'.format(self.name, k))
+        logger.info('[{}] Shared Secret ss={}'.format(self.name, self.k))
 
-        own_message = '{}{}{}{}{}{}'.format(k , self.scalar , peer_scalar , self.element , peer_element[0] , self.mac_address).encode()
-        peer_message = '{}{}{}{}{}{}'.format(k , peer_scalar , self.scalar , peer_element[0] , self.element , peer_mac).encode()
+        own_message = '{}{}{}{}{}{}'.format(self.k , self.scalar , self.peer_scalar , self.element[0] , self.peer_element[0] , self.mac_address).encode()
 
         H = hashlib.sha256()
         H.update(own_message)
         self.token = H.hexdigest()
 
+        return self.token
+
+    def confirm_exchange(self, peer_token):
+        """
+            In the Confirm Exchange, both sides confirm that they derived the
+            same secret, and therefore, are in possession of the same password.
+        """
+        peer_message = '{}{}{}{}{}{}'.format(self.k , self.peer_scalar , self.scalar , self.peer_element[0] , self.element[0] , self.peer_mac).encode()
         H = hashlib.sha256()
         H.update(peer_message)
-        self.peer_token = H.hexdigest()
+        self.peer_token_computed = H.hexdigest()
 
-        logger.info('[{}] Own Token={}'.format(self.name, self.token))
-        logger.info('[{}] Other Token={}'.format(self.name, self.peer_token))
+        logger.info('[{}] Computed Token from Peer={}'.format(self.name, self.peer_token_computed))
+        logger.info('[{}] Received Token from Peer={}'.format(self.name, peer_token))
 
+        # Pairwise Master Key” (PMK)
+        # compute PMK = H(k | scal(AP1) + scal(AP2) mod q)
+        pmk_message = '{}{}'.format(self.k, (self.scalar + self.peer_scalar) % self.q).encode()
+        H = hashlib.sha256()
+        H.update(pmk_message)
+        self.PMK = H.hexdigest()
+
+        logger.info('[{}] Pairwise Master Key(PMK)={}'.format(self.name, self.PMK))
 
     def key_derivation_function(self, n, base, seed):
         """
@@ -422,18 +488,48 @@ def handshake():
     sta = Peer('abc1238', mac1, 'STA')
     ap = Peer('abc1238', mac2, 'AP')
 
+    logger.info('Starting hunting and pecking to derive PE...\n')
+
     sta.initiate(mac2)
     ap.initiate(mac1)
 
+    print()
     logger.info('Starting dragonfly commit exchange...\n')
 
     scalar_sta, element_sta = sta.commit_exchange()
     scalar_ap, element_ap = ap.commit_exchange()
 
+    print()
     logger.info('Computing shared secret...\n')
 
-    sta.compute_shared_secret(element_ap, scalar_ap, mac2)
-    ap.compute_shared_secret(element_sta, scalar_sta, mac1)
+    sta_token = sta.compute_shared_secret(element_ap, scalar_ap, mac2)
+    ap_token = ap.compute_shared_secret(element_sta, scalar_sta, mac1)
+
+    print()
+    logger.info('Confirm Exchange...\n')
+
+    sta.confirm_exchange(ap_token)
+    ap.confirm_exchange(sta_token)
+
+def tests():
+    """
+    Test the fucking Curve class.
+
+    See Understanding Cryptography ECC Section.
+    """
+    a, b, p = 2, 2, 17
+    curve = Curve(a, b, p)
+
+    P = Point(5, 1)
+    assert curve.double_add_algorithm(19, P) == O
+
+    T = P
+    for i in range(p+1):
+        T = curve.ec_add(T, P)
+
+    assert curve.double_add_algorithm(19, P) == T
+
 
 if __name__ == '__main__':
+    #tests()
     handshake()
